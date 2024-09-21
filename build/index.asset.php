7026fc0c10b098cd36ffa3f7438b69f01ac1b23b
hmbashar <hmbashar@gmail.com>
2024-09-21 12:59:03 +0600

    align

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element'), 'version' => '4bc84c27944852d5bbd3');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element'), 'version' => '3f7f125daad45a1b77ec');
